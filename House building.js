79ed008be970b90f9171e999dbd5cd05fe3f4052
MattyDaddy24 <mathiasbj24@gmail.com>
2022-03-16 12:56:00 +0100

    moneymoney

--- House building.js
+++ House building.js
@@ -6,38 +6,48 @@ let placed2 = false
 let placed3 = false
 let placed4 = false
 let placed5 = false
+timesdrawn = 0
 
+
+//Sørger for at spillet ved når et af husende er placeret: 
 function draw() {
     if (placed1 = true && HousesUsed < 1) {
-        rd.add('placednow', () => available - 5 && HousesUsed + 1)
+        rd.add('placednow', () => available - 1 && HousesUsed + 1);
+        if(timesdrawn = 0){
+        drawmoney();
+        }
         
     }
 }
 
 function draw() {
     if (placed2 = true && HousesUsed < 2) {
-        rd.add('placednow', () => available - 5 && HousesUsed + 1)
+        rd.add('placednow', () => available - 1 && HousesUsed + 1)
         
     }
 }
 
 function draw() {
-    if (placed1 = true && HousesUsed < 3) {
-        rd.add('placednow', () => available - 5 && HousesUsed + 1)
+    if (placed3 = true && HousesUsed < 3) {
+        rd.add('placednow', () => available - 1 && HousesUsed + 1)
         
     }
 }
 
 function draw() {
-    if (placed1 = true && HousesUsed < 4) {
-        rd.add('placednow', () => available - 5 && HousesUsed + 1)
+    if (placed4 = true && HousesUsed < 4) {
+        rd.add('placednow', () => available - 1 && HousesUsed + 1)
         
     }
 }
 
 function draw() {
-    if (placed1 = true && HousesUsed < 5) {
-        rd.add('placednow', () => available - 5 && HousesUsed + 1)
+    if (placed4 = true && HousesUsed < 5) {
+        rd.add('placednow', () => available - 1 && HousesUsed + 1)
         
     }
 }
+
+
+
+
